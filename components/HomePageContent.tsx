eecd90e8e4aa9b4bd8e52c5ab1519dcc705afeb2
amit <amitshetty60198@gmail.com>
2026-07-02 12:33:35 +0530

    bottom: swap 3 squares for 1 rectangle using same type as top

--- components/HomePageContent.tsx
+++ components/HomePageContent.tsx
@@ -114,7 +114,8 @@ export function HomePageContent() {
         </article>
       </section>
 
-      <div className="mt-10 flex justify-center">
+      <div className="mt-10 flex flex-col items-center gap-4">
+        <AdsterraAd type="rectangle" />
         <AdsterraAd type="rectangle" />
       </div>
 
@@ -162,9 +163,7 @@ export function HomePageContent() {
       </div>
 
       <div className="mt-12 flex flex-wrap justify-center gap-4">
-        <AdsterraAd type="square" />
-        <AdsterraAd type="square" />
-        <AdsterraAd type="square" />
+        <AdsterraAd type="rectangle" />
       </div>
 
     </div>
